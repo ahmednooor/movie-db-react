410e0679ef50dbc4e92ef9d1302976d6d3eeaeb3
ahmednooor <m.ahmednoor7@yahoo.com>
2018-04-13 13:40:39 +0500

    changed links for static usage

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,7 +4,7 @@ import './NavBar.css';
 const NavBar = (props) => {
     return (
         <header className="NavBar">
-            <a className="logo" href="/">MOVIE DB REACT</a>
+            <a className="logo" href="index.html">MOVIE DB REACT</a>
             <button 
                 className="ham-icon"
                 dangerouslySetInnerHTML={{__html: `<svg height="32px" id="Layer_1" style="enable-background:new 0 0 32 32;" version="1.1" viewBox="0 0 32 32" width="32px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M4,10h24c1.104,0,2-0.896,2-2s-0.896-2-2-2H4C2.896,6,2,6.896,2,8S2.896,10,4,10z M28,14H4c-1.104,0-2,0.896-2,2  s0.896,2,2,2h24c1.104,0,2-0.896,2-2S29.104,14,28,14z M28,22H4c-1.104,0-2,0.896-2,2s0.896,2,2,2h24c1.104,0,2-0.896,2-2  S29.104,22,28,22z"/></svg>`}}
@@ -27,7 +27,7 @@ const NavBar = (props) => {
                 </button>
                 <nav>
                     <ul>
-                        <li><a href="/">Home</a></li>
+                        <li><a href="index.html">Home</a></li>
                         <li><a href="#/">About</a></li>
                     </ul>
                 </nav>
